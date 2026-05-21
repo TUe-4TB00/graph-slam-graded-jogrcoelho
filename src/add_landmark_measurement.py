68b51a4d8abfd4481eeda7b15dddddcc1e62e838
Joao Gomes Rodrigues Coelho <j.gomes.rodrigues.coelho@student.tue.nl>
2026-05-21 12:28:35 +0200

    Finished Except Bonus

--- src/add_landmark_measurement.py
+++ src/add_landmark_measurement.py
@@ -9,7 +9,7 @@ MEASUREMENT_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.05, 0.1]))  # (
 
 def add_landmark_measurement(graph, initial_estimate, result):
     # Determine the correct rotation (bearing) and distance from X(4) to L(2) 
-    # rotation = 
-    # distance = 
+    rotation = math.degrees(np.pi/2 - ((np.pi - np.pi/4)/2 - np.pi/4))
+    distance = np.sqrt(4 + 4 - 2 * 2 * 2 *np.cos(np.pi/4))
     graph.add(gtsam.BearingRangeFactor2D(X(4), L(2), gtsam.Rot2.fromDegrees(rotation), distance, MEASUREMENT_NOISE))
     return graph
--- src/add_pose.py
+++ src/add_pose.py
@@ -9,8 +9,8 @@ ODOMETRY_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.2, 0.2, 0.1]))  # 
 MEASUREMENT_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.05, 0.1]))  # (bearing, range)
 
 def add_pose(graph, initial_estimate):
-    # TODO: Add the odometry factor between X(4) and X(5) to the graph (BetweenFactorPose2)
-    graph.add(gtsam.BetweenFactorPose2(X(3), X(4), gtsam.Pose2(0.0, 2.0, 0.0), ODOMETRY_NOISE))
-    # TODO: Based on the odometry, find the initial estimate for the pose of X(5) and add it to the graph
-    initial_estimate.insert(X(4), gtsam.Pose2(4.10, 2.10, 0.10))
+    # TODO: Add the odometry factor between X(3) and X(4) to the graph (BetweenFactorPose2)
+    graph.add(gtsam.BetweenFactorPose2(X(3), X(4), gtsam.Pose2(np.sqrt(2),np.sqrt(2), np.pi/2), ODOMETRY_NOISE))
+    # TODO: Based on the odometry, find the initial estimate for the pose of X(4) and add it to the graph
+    initial_estimate.insert(X(4), gtsam.Pose2(4.05 + np.sqrt(2), np.sqrt(2), 0.05 + np.pi/2))
     return graph, initial_estimate
